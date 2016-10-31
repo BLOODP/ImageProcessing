89ccdbe28283868071d10a1e7b7097030510ae2d
heguangqin <1393631507@qq.com>
2016-10-31 23:39:42 +0800

    opencv 二值化处理以及bitwise

--- test-opencv.py
+++ test-opencv.py
@@ -2,16 +2,10 @@ import cv2
 import numpy as np
 
 img = cv2.imread('/Users/heguangqin/Pictures/source_1.jpg',cv2.IMREAD_COLOR)
-
-
-
 print type(img),img.shape
 
 mat = np.mat([[255,100,10],[100,50,200]])
 print type(mat)
-# cv2.imshow('img',mat)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 img2gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 print img2gray.shape
 print type(img2gray)
